45c8983214fcf8b252365e20d3d9b95a5226ccf3
saifullah <saifullah1902743@st.jmi.ac.in>
2024-10-11 21:23:25 +0530

    Update router and fixed deployement issue

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
-import {BrowserRouter, Routes, Route} from "react-router-dom";
+import {HashRouter as Router, Routes, Route} from "react-router-dom";
 import { Dashboard, Home, SharedLayout } from "./pages";
 
 function App() {
   return (
-    <BrowserRouter>
+    <Router>
     <Routes>
       <Route path="/" element={<SharedLayout/>}>
         <Route index element={<Home/>}/>
         <Route path="dashboard" element={<Dashboard/>}/>
       </Route>
     </Routes>
-    </BrowserRouter>
+    </Router>
   );
 }
 
